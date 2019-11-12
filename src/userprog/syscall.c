1da1e54501fcac6705d9f0a6a99f9cd4b92416d0
raararaara <cse20141574@cspro9.sogang.ac.kr>
2019-11-12 01:09:54 -0800

    change eax = -1 into fatal

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -160,8 +160,7 @@ syscall_handler (struct intr_frame *f)
     const char* filename = *(char **)((int *)f->esp + 1);
 
     if (!check_user_string(filename)) {
-      f->eax = -1;
-      break;
+		goto fatal;
     } 
     
     struct thread* cur = thread_current();
@@ -216,8 +215,7 @@ syscall_handler (struct intr_frame *f)
     int fd = *(int*)((char*)f->esp + 4);   
 
     if (!check_range((uint8_t*)buffer, (uint8_t*)buffer + size)) {
-      f->eax = -1;
-      break;
+		goto fatal;
     }
     if (fd == 0) {
       unsigned i; 
@@ -251,8 +249,7 @@ syscall_handler (struct intr_frame *f)
     int fd = *(int*)((char*)f->esp + 4);
 
     if (!check_range(buffer, buffer + size)) {
-      f->eax = -1;
-      break;
+		goto fatal;
     }
     if (fd == 1) {
       putbuf(buffer, size);
